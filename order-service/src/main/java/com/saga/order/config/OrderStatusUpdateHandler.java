219a907513ef4f31ad865426bf4bf1261aab9b00
rathinavelkolanjinathan <123318472+rathinavelkolanjinathan@users.noreply.github.com>
2025-03-09 16:26:41 +0530

    Successfully started app

---
+++ order-service/src/main/java/com/saga/order/config/OrderStatusUpdateHandler.java
@@ -0,0 +1,47 @@
+package com.saga.order.config;
+
+import com.saga.commons.dto.OrderRequestDto;
+import com.saga.commons.events.OrderStatus;
+import com.saga.commons.events.PaymentStatus;
+import com.saga.order.entity.PurchaseOrder;
+import com.saga.order.repository.OrderRepository;
+import com.saga.order.service.OrderStatusPublisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.function.Consumer;
+
+@Configuration
+public class OrderStatusUpdateHandler {
+
+    @Autowired
+    private OrderRepository repository;
+
+    @Autowired
+    private OrderStatusPublisher publisher;
+
+    @Transactional
+    public void updateOrder(int id, Consumer<PurchaseOrder> consumer) {
+        repository.findById(id).ifPresent(consumer.andThen(this::updateOrder));
+    }
+
+    private void updateOrder(PurchaseOrder purchaseOrder) {
+        boolean isPaymentComplete = PaymentStatus.PAYMENT_COMPLETED.equals(purchaseOrder.getPaymentStatus());
+        OrderStatus orderStatus = isPaymentComplete ? OrderStatus.ORDER_COMPLETED : OrderStatus.ORDER_CANCELLED;
+        purchaseOrder.setOrderStatus(orderStatus);
+        if (!isPaymentComplete) {
+            publisher.publishOrderEvent(convertEntityToDto(purchaseOrder), orderStatus);
+        }
+    }
+
+    public OrderRequestDto convertEntityToDto(PurchaseOrder purchaseOrder) {
+        OrderRequestDto orderRequestDto = new OrderRequestDto();
+        orderRequestDto.setOrderId(purchaseOrder.getId());
+        orderRequestDto.setUserId(purchaseOrder.getUserId());
+        orderRequestDto.setAmount(purchaseOrder.getPrice());
+        orderRequestDto.setProductId(purchaseOrder.getProductId());
+        return orderRequestDto;
+    }
+}
